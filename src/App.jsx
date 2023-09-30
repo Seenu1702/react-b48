8583e794000f74b190f49a9e50b063f1cc49dcdc
Seenu1702 <seenusubramaniyan17@gmail.com>
2023-09-30 19:17:35 +0530

    added prop + inline styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-const Hello = () =>
+const Hello = (props) =>
+// console.log(props);
 {
   return (
     <div>
-      <p>Hello World!</p>
+      <p>Hello { props.name}, your age is { props.age}</p>
     </div>
   )
 }
@@ -11,9 +12,10 @@ const App = () =>{
   return (
     
     <div>
-      <h1>Greetings</h1>
-      <Hello />
-      {/* <Hello /> this will be reusabele componenet */}
+      <h1 style={{ color:'white', background:'red'}}>Greetings</h1>
+      <Hello name='Seenu' age='24'/>
+      <Hello name='Seenu' age='24'/>
+      {/* <Hello /> this will be reusabele componenet same like functions  */}
     </div>
   )
 
